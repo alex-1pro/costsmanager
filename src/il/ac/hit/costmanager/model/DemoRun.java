9b27bba2634f546d7bb9ce3dc1b12f06511c143a
alex-1pro <pro1.alex.b@gmail.com>
2020-07-09 10:38:30 +0300

    User Dao hibernate done

--- src/il/ac/hit/costmanager/model/DemoRun.java
+++ src/il/ac/hit/costmanager/model/DemoRun.java
@@ -4,20 +4,14 @@ package il.ac.hit.costmanager.model;
 
 public class DemoRun {
 	 public static void main(String[] args) {
-		 User user =new User ("Alex","123456");
+		
 		 try {
 			 IUserDAO dao=UserDAOHibernet.getInstance();
-			// boolean success =false;
-				// success=dao.userRegister("Alex", "123456");
-			 	//dao.userRegister("aaa", "bbbb");
-			 	//dao.userRegister("cccc", "gggg");
-			 	dao.userRegister("jjjk", "kkkk");
-			 	dao.userRegister("fffff", "kkkk");
-
-				/*
-				 * if(success) System.out.println(user+" added successfully\n\n"); else
-				 * System.out.println(user+" did'nt add\n\n");
-				 */
+			
+			 dao.userRegister("Alex", "123456");
+			 User u2=dao.validateUser("Alex", "123456");
+			 System.out.println(u2+"\n\n");
+				
 		 }catch (UserCostsManagerException e) {
 			e.printStackTrace();
 		}
--- src/il/ac/hit/costmanager/model/UserDAOHibernet.java
+++ src/il/ac/hit/costmanager/model/UserDAOHibernet.java
@@ -22,27 +22,19 @@ public class UserDAOHibernet implements IUserDAO{
 
 	        return instance;
 	  }
-	 
-	 
-	 
-
 	@Override
-	public boolean userRegister(String userName, String password) throws UserCostsManagerException{
-		// TODO Auto-generated method stub
-		
-	      Session session = null;
+	public User userRegister(String userName, String password) throws UserCostsManagerException {
+		 Session session = null;
 	        User user = null;
 	        try {
 	            session = factory.openSession();
 	            session.beginTransaction();
-	            if(!checkUserName(userName)) {
+	            if(!checkUserName(userName))
 	                throw new UserCostsManagerException("An attempt to register userName: '" + userName +"' was done, user already exists.");
-	                
-	            }
-	            User u = new User(userName,password);
-	            session.save(u);
+	            User userTemp = new User(userName, password);
+	            session.save(userTemp);
 	            session.getTransaction().commit();
-	            Query query = session.createQuery("from User U where U.username= :username")
+	            Query query = session.createQuery("from User U where U.userName= :username")
 	                    .setString("username",userName);
 	            List<?> users = query.list();
 	            Iterator<?> i = users.iterator();
@@ -60,53 +52,76 @@ public class UserDAOHibernet implements IUserDAO{
 	            if(session!=null)
 	                session.close();
 	        }
-	      //  return user;
+	        return user;
 		
-		return false;
+	}
+	
+	
+	
+	@Override
+	public User validateUser(String userName, String password) throws UserCostsManagerException {
+		Session session = null;
+        User user = null;
+        try {
+            session = factory.openSession();
+            session.beginTransaction();
+            //Checks if the user exists or details are valid
+            Query query=session.createQuery("FROM User U WHERE U.userName = :username and U.password= :password")
+                    .setString("username",userName)
+                    .setString("password",password);
+            List<?> users = query.list();
+            // If the information sent doesn't fit a row in the DB
+            if (users.size() == 0)
+                throw new UserCostsManagerException("Username '" + userName +"' is not valid or wrong password");
+            else
+                user = (User)users.get(0);
+        }
+        catch (HibernateException e)
+        {
+            Transaction tx = Objects.requireNonNull(session).getTransaction();
+            if (tx.isActive()) tx.rollback();
+        }
+        finally
+        {
+            if(session!=null) session.close();
+        }
+        return user;
 	}
 	
 	
 	
 	@Override
 	public boolean checkUserName(String username) throws UserCostsManagerException {
-		
-		
-		Session session = null;
-        try
-        {
-            session = factory.openSession();
-            session.beginTransaction();
+		 Session session = null;
+	        try
+	        {
+	            session = factory.openSession();
+	            session.beginTransaction();
 
-            Query query = session.createQuery("FROM User U WHERE U.userName = :username")
-                    .setString("username",username);
-            List<?> users = query.list();
-            if(users.size() != 0)
-                return false;
-        }
-        catch (HibernateException e)
-        {
-            Transaction tx = Objects.requireNonNull(session).getTransaction();
-            if (tx.isActive())
-                tx.rollback();
-        }
-        finally
-        {
-            if(session!=null) session.close();
-        }
-        return true;
-    }
-		
-	
-	@Override
-	public boolean validateUser(String userName, String password) throws UserCostsManagerException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public User getUser(String userName, String password) throws UserCostsManagerException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	            Query query = session.createQuery("FROM User U WHERE U.userName = :username")
+	                    .setString("username",username);
+	            List<?> users = query.list();
+	            if(users.size() != 0)
+	                return false;
+	        }
+	        catch (HibernateException e)
+	        {
+	            Transaction tx = Objects.requireNonNull(session).getTransaction();
+	            if (tx.isActive())
+	                tx.rollback();
+	        }
+	        finally
+	        {
+	            if(session!=null) session.close();
+	        }
+	        return true;
+	    }
 	
 }
+	 
+	 
+	 
+
+	
+	
+
